dd89664d757539faa188a8d3ecd4183fa0fe8c0d
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-03-14 00:34:50 -0500

    feat: entidades completas (de momento)

--- src/main/java/com/chamo/chamomarket/entity/CategoryEntity.java
+++ src/main/java/com/chamo/chamomarket/entity/CategoryEntity.java
@@ -1,7 +1,21 @@
 package com.chamo.chamomarket.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import java.util.ArrayList;
 
 @Entity
 public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "status", nullable = false)
+    private Boolean status;
+
+    @OneToMany(mappedBy = "category")
+    private ArrayList<ProductEntity> products;
 }
