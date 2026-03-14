7ef0409fb3992b346a258cd731775422514a00ca
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-03-13 21:12:31 -0500

    First Commit

---
+++ src/main/java/com/chamo/chamomarket/ChamoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.chamo.chamomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamoMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChamoMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chamo/chamomarket/ChamoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamo.chamomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamoMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
