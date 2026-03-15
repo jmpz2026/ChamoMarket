8bc18cbac03aa891c6373eac0fdf44ba358c120f
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-03-14 20:48:58 -0500

    feat: Se añadio una inyeccion de los repositorios en sus servicios respectivos

--- src/main/java/com/chamo/chamomarket/service/CategoryService.java
+++ src/main/java/com/chamo/chamomarket/service/CategoryService.java
@@ -1,7 +1,12 @@
 package com.chamo.chamomarket.service;
 
+import com.chamo.chamomarket.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
 }
--- src/main/java/com/chamo/chamomarket/service/ProductService.java
+++ src/main/java/com/chamo/chamomarket/service/ProductService.java
@@ -1,7 +1,12 @@
 package com.chamo.chamomarket.service;
 
+import com.chamo.chamomarket.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
 }
