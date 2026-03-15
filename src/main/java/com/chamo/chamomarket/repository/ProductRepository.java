fa4c062414070543907ca3bee6f3c6623c85b77b
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-03-14 20:46:54 -0500

    feat: se cambiaron los repository de clases a interfaces y se agrego la notacion de @Repository

--- src/main/java/com/chamo/chamomarket/repository/ProductRepository.java
+++ src/main/java/com/chamo/chamomarket/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.chamo.chamomarket.repository;
 
-public class ProductRepository {
+import com.chamo.chamomarket.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
 }
