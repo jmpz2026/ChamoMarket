33aa25c544364316af4635f5b6aeea6f0b25807d
McamilaBonilla <mariac.bonillam@uqvirtual.edu.co>
2026-05-12 14:57:17 -0500

    feat: agregar registro y prueba en postman

--- src/main/java/com/chamo/chamomarket/controller/AuthController.java
+++ src/main/java/com/chamo/chamomarket/controller/AuthController.java
@@ -3,12 +3,17 @@ package com.chamo.chamomarket.controller;
 import com.chamo.chamomarket.dto.auth.AuthRequestDTO;
 import com.chamo.chamomarket.dto.auth.AuthResponseDTO;
 import com.chamo.chamomarket.dto.auth.GenerateTokenRequestDTO;
+import com.chamo.chamomarket.dto.auth.RegisterRequestDTO;
 import com.chamo.chamomarket.dto.auth.TokenDataDTO;
 import com.chamo.chamomarket.dto.auth.TokenResponseDTO;
+
 import com.chamo.chamomarket.service.AuthService;
 import com.chamo.chamomarket.service.JwtService;
+
 import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -29,19 +34,24 @@ public class AuthController {
 
     @PostMapping("/generate")
     public TokenResponseDTO generate(@RequestBody GenerateTokenRequestDTO request) {
+
         String token = jwtService.generateToken(
                 request.getEmployeeId(),
                 request.getRole(),
                 request.getUsername()
         );
+
         return new TokenResponseDTO(token);
     }
 
     @PostMapping("/validate")
     public Map<String, Boolean> validate(@RequestBody TokenResponseDTO request) {
+
         boolean valid = jwtService.isTokenValid(request.getToken());
+
         Map<String, Boolean> response = new HashMap<>();
         response.put("valid", valid);
+
         return response;
     }
 
@@ -52,7 +62,22 @@ public class AuthController {
 
     @PostMapping("/refresh")
     public TokenResponseDTO refresh(@RequestBody TokenResponseDTO request) {
+
         String newToken = jwtService.refreshToken(request.getToken());
+
         return new TokenResponseDTO(newToken);
+    }
+
+    @PostMapping("/register")
+    public Map<String, String> register(
+            @RequestBody @Valid RegisterRequestDTO request
+    ) {
+
+        authService.register(request);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Usuario registrado correctamente");
+
+        return response;
     }
 }
