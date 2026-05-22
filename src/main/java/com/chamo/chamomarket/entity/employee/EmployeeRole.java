4d4dfb415bfdc99b65f09ceaaeaee33a94693916
McamilaBonilla <mariac.bonillam@uqvirtual.edu.co>
2026-05-22 16:41:01 -0500

    Subiendo avances del modulo de Camila

--- src/main/java/com/chamo/chamomarket/entity/employee/EmployeeRole.java
+++ src/main/java/com/chamo/chamomarket/entity/employee/EmployeeRole.java
@@ -1,6 +1,15 @@
 package com.chamo.chamomarket.entity.employee;
 
+import lombok.Getter;
+
+@Getter
 public enum EmployeeRole {
-    ADMINISTRADOR,
-    CAJERO
-}
+    ADMINISTRADOR(1L),
+    CAJERO(2L);
+
+    private final long id;
+
+    EmployeeRole(long id) {
+        this.id = id;
+    }
+}v
